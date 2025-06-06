7854b2cbb6baf47fbcff887cb7f3160736349da1
Daniel Coppia Nelson <dcoppianelson@gmail.com>
2025-06-06 10:39:57 -0400

    version1
    Primera versión de mi código, con librería random

---
+++ Creación_números_aleatorios.py
@@ -0,0 +1,5 @@
+import random   #Librería para llamar y crear números al azar
+#También se puede escribir 'from random import randint
+numero=random.randint(1,100)    #Rango para el número aleatorio
+
+print(numero)
